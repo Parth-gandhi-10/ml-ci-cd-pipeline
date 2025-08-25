75cf85fd8fb4669b10885c9138daa589d504ed48
Parth Gandhi <parthgandhi246@gmail.com>
2025-08-25 11:00:56 +0530

    Intial commit 1.3

--- model/train.py
+++ model/train.py
@@ -14,8 +14,8 @@ y = data['species']
 #split into training and testing 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
 
-#train the Random classifer model
-model = RandomForestClassifer(n_estimators=100, random_state=42)
+#train the Random classifier model
+model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 #Save the model 
