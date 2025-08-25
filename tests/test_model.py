f5360cc4d9399e7f10e8a125acc54329660c5b8e
Parth Gandhi <parthgandhi246@gmail.com>
2025-08-25 11:17:48 +0530

    Intial commit 1.5

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,5 +7,5 @@ class TestModelTraining(unittest.TestCase):
 		model= joblib.load('model/iris_model.pkl')
 		self.assertIsInstance(model, RandomForestClassifier)
 		self.assertGreaterEqual(len(model.feature_importances_),4)
-if__name__=='__main__':
+if __name__=='__main__':
 	unittest.main()
