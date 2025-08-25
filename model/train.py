922f15a26255d1869cd9171d5605fa49b1ca547c
Parth Gandhi <parthgandhi246@gmail.com>
2025-08-25 10:44:30 +0530

    Intial commit 1.2

--- model/train.py
+++ model/train.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifer
+from sklearn.ensemble import RandomForestClassifier
 import joblib 
 
 #load the dataset
