72b7f3bada590ef07d4a00a13201bf61c14a0589
Parth Gandhi <parthgandhi246@gmail.com>
2025-08-25 11:14:45 +0530

    Intial commit 1.4

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,8 +1,8 @@
 import unittest 
 import joblib
-from sklearn.ensemble import RandomForestclassifier
+from sklearn.ensemble import RandomForestClassifier
 
-class TestModelTraining(unittest.Testcase):
+class TestModelTraining(unittest.TestCase):
 	def test_model_training(self):
 		model= joblib.load('model/iris_model.pkl')
 		self.assertIsInstance(model, RandomForestClassifier)
